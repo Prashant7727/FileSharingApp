5e047ec0a526e72b2d39eb8c1e5156cde4bff0af
Prashant7727 <prashant.sisodia08@gmail.com>
2024-07-09 21:22:14 +0530

    changes to find an error in api.js

--- src/service/api.js
+++ src/service/api.js
@@ -7,6 +7,19 @@ export const uploadFile = async (data) => {
         const response = await axios.post(`${API_URI}/upload`, data);
         return response.data;
     } catch (error) {
-        console.log('Error while calling the API ', error.message);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log('Error response data:', error.response.data);
+            console.log('Error response status:', error.response.status);
+            console.log('Error response headers:', error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log('Error request:', error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error message:', error.message);
+        }
+        console.log('Error config:', error.config);
     }
 }
